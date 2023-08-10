a3faa6b0658d5bfa562c862b63a0f9d2ca273cf7
Mohamed <moelobeid@gmail.com>
2023-08-10 20:40:27 +0200

    fixed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - starting point of program
+*
+* Description:  prints the statement
+*
+*  Return: 0 alwaays
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
