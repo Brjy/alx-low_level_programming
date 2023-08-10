ed5f751db40fb931048366f5ecb46d1de0c83647
Mohamed <moelobeid@gmail.com>
2023-08-10 23:18:18 +0200

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 /*
 * main - starting point of program
 *
-* Description: lkdfj ljflkdj kfjdlk
+* Description: lkdfj flkdj kfjdlk
 *
 * Return: always return value 0f 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
 
