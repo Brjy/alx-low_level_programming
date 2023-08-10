0c0045fd223faa0c9ce9c177fe59ed538ea1c731
Mohamed <moelobeid@gmail.com>
2023-08-10 20:33:33 +0200

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+* main starting point of program
+*
+* description  prints the statement 
+*
+*  return 0 alwaays
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
