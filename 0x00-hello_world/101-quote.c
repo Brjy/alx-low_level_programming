adf2b048698890d9430f57ad3d47bede4825b1d8
Mohamed <moelobeid@gmail.com>
2023-08-11 13:37:48 +0200

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
+#include<stdio.h>
 #include<unistd.h>
-/*
-* main - starting point of program
-*
-* Description: lkdfj flkdj kfjdlk
-*
-* Return: always return value 0f 1
-*/
+/**
+ * main - starting point of program here.
+ * Description: ths is a program thats prints without printf,puts.
+ * Return: always return value 0f 1.
+ */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
 }
 
