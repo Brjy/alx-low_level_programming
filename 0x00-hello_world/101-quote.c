8ba58841e682b90bf24a9cc0816c20237a75b73b
Mohamed <moelobeid@gmail.com>
2023-08-11 14:14:08 +0200

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include<unistd.h>
+#include<stdio.h>
 /**
 *main - Entry point of program
 *
-*Description: a code thats print string without printf, puts functions
+*Description: code thats print string without printf, puts functions
 *
 *Return: always return the value of 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
