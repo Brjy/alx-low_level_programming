7e00cc62ae6c86e857c0c7fe3d43ca9c057d77ab
Mohamed <moelobeid@gmail.com>
2023-08-14 07:35:34 +0200

    fixed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main- entry point of program here.
+* Description: this is a program that print negative pr posative integer
+* Return: always return zero.
+**/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
