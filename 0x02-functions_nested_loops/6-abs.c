49b49f61c9f096d415e14c22ec9143e2afb827f7
Mohamed <moelobeid@gmail.com>
2023-08-16 00:19:18 +0200

    fixed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - cheks sign of number and if greater or less than 0.
+* @int: number to check
+* Description: this programm prints '_putchar'
+* Return: return 1 for letters and 0 for else
+**/
+int _abs(int)
+{
+	if (int < 0)
+		return (int * -1);
+	else
+		return (int);
+}
