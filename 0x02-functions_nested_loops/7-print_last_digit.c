7143ffb6f3eacd69350ad91fb9f58efca885d81d
Mohamed <moelobeid@gmail.com>
2023-08-16 00:27:12 +0200

    fixed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_last_digit- cheks sign of number and if greater or less than 0.
+* @n: number to check
+* Description: this programm prints '_putchar'
+* Return: return 1 for letters and 0 for else
+**/
+int print_last_digit(int n);
+{
+	int x = n % 10;
+
+	_putchar('x');
+}
